de60e2c659ff070904d858190c177baddad0f63e
Antoinette <42301441+antoinettemarie@users.noreply.github.com>
2019-02-16 18:17:47 +0600

    Update sqrt.scm

--- sqrt.scm
+++ sqrt.scm
@@ -1,4 +1,4 @@
 (define improve (lambda (guess x) (/ (+ guess (/ x guess)) 2)))
 (define good-enough? (lambda (guess x) (< (- (* guess guess) x) 0.000001)))
 (define sqrt-iter (lambda (guess x) (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x))))
-(definsqrt (lambda (x) (sqrt 1.0 x)))
+(define sqrt (lambda (x) (sqrt 1.0 x)))
