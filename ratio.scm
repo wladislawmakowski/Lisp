556a00b3170fae5af26c9f4d1e89097a0cd1a76a
Antoinette <42301441+antoinettemarie@users.noreply.github.com>
2019-02-20 18:50:53 +0600

    Update ratio.scm

--- ratio.scm
+++ ratio.scm
@@ -1,8 +1,8 @@
-; ***************************************************************
-; It is just simple library for the rational numbers operations *
-; ***************************************************************
-
-; Here's definition of a pair constructor and its selectors
+; ***************************************************************     ********************************* L ****
+; It is just simple library for the rational numbers operations ******* #N is the abstraction barriers  ↓    *
+; ***************************************************************     ********************************* H  ***
+;                                                                                                      *****
+; Here's definition of a pair constructor and its selectors                                                   #1
 
 (define (twin a b)
   (lambda (pick)
@@ -12,13 +12,13 @@
 (define (xcor x) (x 0))
 (define (ycor x) (x 1))
 
-; Here's the constructor for rational numbers and its selectors
+; Here's the constructor for rational numbers and its selectors                                                #2
 
 (define (make-RAT n d) (twin n d))
 (define (num x) (let ((g (gcd (xcor x) (ycor x)))) (/ (xcor x) g)))
 (define (denom x) (let ((g (gcd (xcor x) (ycor x)))) (/ (ycor x) g)))
 
-; And here is definition of sum, substraction, division and multiplication for rational numbers 
+; And here is definition of sum, substraction, division and multiplication for rational numbers                #3
 
 (define (+RAT x y)
   (make-RAT (+ (* (num x) (denom y)) (* (num y) (denom x)))
