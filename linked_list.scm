81add9a7aedceba7ef94614483c39ea5ac75eece
Antoinette <42301441+antoinettemarie@users.noreply.github.com>
2019-02-23 22:41:47 +0600

    Update linked_list.scm

--- linked_list.scm
+++ linked_list.scm
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname linked_list) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (define (node x y)
   (lambda (x) (cond ((= x 0) x)
                     ((= x 1) y))))
@@ -13,4 +10,3 @@
       null
       (node (f (carr l))
         (mapp f (cdrr l)))))
-
