d1c44cc647174538d4f310c8a244d44e4f9b8044
Antoinette <marieantoinette98775420@yahoo.com>
2019-02-24 13:22:53 +0600

    cs

--- linked_list.scm
+++ linked_list.scm
@@ -2,8 +2,8 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname linked_list) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (define (node x y)
-  (lambda (x) (cond ((= x 0) x)
-                    ((= x 1) y))))
+  (lambda (z) (cond ((= z 0) x)
+                    ((= z 1) y))))
 
 (define (carr x) (x 0))
 (define (cdrr x) (x 1))
