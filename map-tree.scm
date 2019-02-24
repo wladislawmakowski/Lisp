382edd238dac74bdb24a3e99a94465ce4a564d12
Antoinette <42301441+antoinettemarie@users.noreply.github.com>
2019-02-24 13:33:06 +0600

    Update map-tree.scm

--- map-tree.scm
+++ map-tree.scm
@@ -1,5 +1,5 @@
 (define (map-tree func tree)
-  (cond ((null? tree) null)
-        ((not (pair? tree)) (func tree))
-        (else (cons (map-tree func (car tree))
+  (cond ((null? tree) null)                             ; if node is null return null
+        ((not (pair? tree)) (func tree))                ; if node is not a pair apply function
+        (else (cons (map-tree func (car tree))          
                     (map-tree func (cdr tree))))))
