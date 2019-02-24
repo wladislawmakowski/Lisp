814321a135f8368a428f776956a2eb4f2580dd4f
Antoinette <42301441+antoinettemarie@users.noreply.github.com>
2019-02-24 13:29:37 +0600

    Update for-each.scm

--- for-each.scm
+++ for-each.scm
@@ -1,9 +1,4 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname for-each) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (define (foreach f l)
   (cond ((null? l) 0)
         ((f (car l))
          (foreach f (cdr l)))))
-
-
